dfd1f00b41d1ca1b5e948e1cdfdd4f980d120a95
Branea Alexandru <55286405+braneaa@users.noreply.github.com>
2021-03-08 12:49:02 +0200

    ssvv lab2

---
+++ SSVV/mavenproject/src/test/java/TestApp.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class TestApp {
+
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2(){
+        assertFalse(false);
+    }
+}
